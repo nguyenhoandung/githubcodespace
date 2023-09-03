5e7a0bc3a74e19beff6414d7f235db21969901e1
Tự tin code giỏi hơn những thằng tán em <126393227+VinDaiDe@users.noreply.github.com>
2023-09-03 19:43:51 +0700

    Update cline1.sh

--- cline1.sh
+++ cline1.sh
@@ -1,4 +1,3 @@
-sudo su
 apt update && apt install qemu-kvm -y &&
 qemu-img create -f raw filegz.img 20G && clear
 read -p "Nhập link file Gz: " url && wget -O- --no-check-certificate "$url" | gunzip | dd of=filegz.img && clear && curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok -y &&
